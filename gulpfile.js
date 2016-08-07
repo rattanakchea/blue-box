9a19d7412615c459e7751691fdf5463aeb554354
Rattanak Chea <rattanak.chea@gmail.com>
2016-08-07 10:45:50 -0700

    set up basic gulp and npm

---
+++ gulpfile.js
@@ -0,0 +1,59 @@
+/*global require*/
+(function(r) {
+    "use strict";
+    var sass = r("gulp-sass");
+    var gulp = r("gulp");
+    //var browserSync = r('browser-sync').create;
+
+    var server = r('gulp-server-livereload');
+  
+
+    var APP = {
+    	scss: ['app/scss/**/*.scss', 'app/scss/*.scss'],
+        css: 'app/css'
+    };
+
+    var DIST = {
+    	css: 'dist/css'
+    };
+
+
+    //scss() is function covert from scss to css
+    //dependend: sass lib
+    gulp.task("scss", function() {
+        return gulp.src( APP.scss
+      	)
+        .pipe(sass())
+        .pipe(gulp.dest(APP.css));
+    });
+
+   
+
+
+    //watch all file changes
+    gulp.task('watch', function(){
+      gulp.watch(APP.styles, ['scss']); 
+      
+      //other watchers
+    });
+
+    gulp.task('webserver', function() {
+      gulp.src('app/')
+        .pipe(server({
+          livereload: true,
+          open: true
+        }));
+    });
+
+    //activate by running `gulp` in terminal
+    // ['here'] is dependency and will be run before gulp.start
+    gulp.task('default', [], function() {
+        gulp.start('scss');
+        gulp.start('watch');
+
+        gulp.start('webserver');
+      
+    });
+
+
+}(require));
